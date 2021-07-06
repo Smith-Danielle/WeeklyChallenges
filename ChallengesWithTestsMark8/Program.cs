8db7a681fbd9888ea9e1af91d38bd977e017403b
Smith-Danielle <smith.ddms@gmail.com>
2021-07-06 17:44:13 -0500

    Updated Set 03

--- ChallengesWithTestsMark8/Program.cs
+++ ChallengesWithTestsMark8/Program.cs
@@ -7,13 +7,7 @@ namespace ChallengesWithTestsMark8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-            ChallengesSet03 test = new ChallengesSet03();
-            string[] test123 = new string[] { "ab1", "de!" };
-            test.ChangeAllElementsToUppercase(test123);
-            foreach (var item in test123)
-            {
-                Console.WriteLine(item);
-            }
+            
         }
     }
 }
